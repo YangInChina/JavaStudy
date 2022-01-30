5bf4fe275a6f92cd67ec9e5def73dd3948f9c18f
BoYang_Zhang <91548907+YangInChina@users.noreply.github.com>
2022-01-30 22:23:40 +0800

    Delete ch02 directory

--- ch02/Demo02.java
+++
@@ -1,9 +0,0 @@
-package base;
-public class Demo02 {
-    public static void main(String[] args) {
-        String a = "hello";
-        int num = 10;
-        System.out.println(a);
-        System.out.println(num);
-    }
-}
